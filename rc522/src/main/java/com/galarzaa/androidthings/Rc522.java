de26c776dbc5da3dfb8e648feb7a146c304c2c43
Allan Galarza <allan.galarza@gmail.com>
2017-07-11 15:05:42 -0700

    Added static methods to calculate access bits and conditions - calculateAccessBits() - calculateAccessConditions()

--- rc522/src/main/java/com/galarzaa/androidthings/Rc522.java
+++ rc522/src/main/java/com/galarzaa/androidthings/Rc522.java
@@ -11,8 +11,8 @@ import java.io.IOException;
  * Based on pi-rc22 by ondryaso
  * https://github.com/ondryaso/pi-rc522/
  *
- * MFRC522 Reference
- * https://www.nxp.com/documents/data_sheet/MFRC522.pdf
+ *
+ * @see <a href="https://www.nxp.com/documents/data_sheet/MFRC522.pdf">MFRC522 Reference</a>
  * @author Allan Galarza
  */
 
@@ -553,14 +553,14 @@ public class Rc522 {
         return getUidString("-");
     }
 
-    private final static char[] HEX_CHARS = "0123456789ABCDEF".toCharArray();
+    private static final char[] HEX_CHARS = "0123456789ABCDEF".toCharArray();
 
     /**
      * Converts a data byte array to a string representing its 16 bytes in hexadecimal
      * @param data the byte array holding the data
      * @return A string representing the block's data
      */
-    static public String dataToHexString(byte[] data){
+    public static String dataToHexString(byte[] data){
         char[] buffer = new char[32];
         for(int i = 0; i < data.length; i++){
             int b = data[i] & 0xFF;
@@ -570,4 +570,97 @@ public class Rc522 {
         return new String(buffer);
     }
 
+    /**
+     * Dumps all the data in all data blocks in MIFARE 1K cards with default authentication keys.
+     * Card must be selected using {@link #selectTag(byte[])} before
+     * This won't work if a sector's KEY A or access bits have been changed
+     * @return string containing all the data
+     */
+    public String dumpMifare1k(){
+        byte[] key = {(byte)0xFF,(byte)0xFF,(byte)0xFF,(byte)0xFF,(byte)0xFF,(byte)0xFF};
+        StringBuilder sb = new StringBuilder();
+        for(byte i = 0; i <= 15; i++){
+            for(byte j = 0; j <= 3; j++){
+                sb.append("S").append(i).append("B").append(j).append(": ");
+                byte block = getBlockAddress(i,j);
+                byte[] buffer = new byte[16];
+                authenticateCard(AUTH_A,block,key);
+                boolean success = readBlock(block,buffer);
+                if(!success){
+                    sb.append("Could not read");
+                }else{
+                    sb.append(dataToHexString(buffer));
+                }
+                sb.append("\n");
+            }
+        }
+        return sb.toString();
+    }
+
+    /***
+     * Calculates the access bits for a sector trailer (bytes 6 to 7) based on Table 6 and Table 7
+     * on MIFARE 1k's reference
+     * @see <a href="http://www.nxp.com/docs/en/data-sheet/MF1S50YYX_V1.pdf#page=12">Reference sheet</a>
+     * @param c1 byte array for the c1 values for block 0, 1, 2 and 3 respectively
+     * @param c2 byte array for the c2 values for block 0, 1, 2 and 3 respectively
+     * @param c3 byte array for the c3 values for block 0, 1, 2 and 3 respectively
+     * @return a 3 byte array containing the access bits
+     */
+    public static byte[] calculateAccessBits(byte[] c1, byte[] c2, byte[] c3){
+        byte[] accessBits = new byte[3];
+        try {
+            // Byte 6
+            accessBits[0] = (byte)(
+                    ((~c2[3] & 1) << 7) + ((~c2[2] & 1) << 6) + ((~c2[1] & 1) << 5) +
+                    ((~c2[0] & 1) << 4) + ((~c1[3] & 1) << 3) + ((~c1[2] & 1) << 2) +
+                    ((~c1[1] & 1) << 1) + (~c1[0] & 1)
+            );
+            // Byte 7
+            accessBits[1] = (byte)(
+                    ((c1[3] & 1) << 7) + ((c1[2] & 1) << 6) + ((c1[1] & 1) << 5) +
+                    ((c1[0] & 1) << 4) + ((~c3[3] & 1) << 3) + ((~c3[2] & 1) << 2) +
+                    ((~c3[1] & 1) << 1) + (~c3[0] & 1)
+            );
+            // Byte 7
+            accessBits[2] = (byte)(
+                    ((c3[3] & 1) << 7) + ((c3[2] & 1) << 6) + ((c3[1] & 1) << 5) +
+                    ((c3[0] & 1) << 4) + ((c2[3] & 1) << 3) + ((c2[2] & 1) << 2) +
+                    ((c2[1] & 1) << 1) + (c2[0] & 1)
+            );
+            return accessBits;
+        }catch(IndexOutOfBoundsException e){
+            return null;
+        }
+    }
+
+    /**
+     * Calculates the access bits for a sector trailer (bytes 6 to 7) based on Table 6 and Table 7
+     * on MIFARE 1k's reference
+     * @param accessConditions a 2d array containing the access conditions for each block
+     * @return a 3 byte array containing the access bits
+     */
+    public static byte[] calculateAccessBits(byte[][] accessConditions){
+        try{
+           return calculateAccessBits(accessConditions[0],accessConditions[1],accessConditions[2]);
+        }catch(IndexOutOfBoundsException e){
+            return null;
+        }
+    }
+
+    /**
+     * Calculates the individual access conditions given the access bits
+     * @param accessBits 3 bytes array containing the access bits of a sector trailer (bytes 6 to 7)
+     * @return a 3-item array representing each of the access conditions (c1,c2,c3), each containing a byte array for the values of each block (0 to 3)
+     */
+    public static byte[][] calculateAccessConditions(byte[] accessBits){
+        try {
+            return new byte[][]{
+                    {(byte)(accessBits[1] >>> 4 & 1), (byte) (accessBits[1] >>> 5 & 1), (byte) (accessBits[1] >>> 6 & 1), (byte) (accessBits[1] >>> 7 & 1)}, //C1
+                    {(byte)(accessBits[2] & 1), (byte) (accessBits[2] >>> 1 & 1), (byte) (accessBits[2] >>> 2 & 1), (byte) (accessBits[2] >>> 3 & 1)}, //C2
+                    {(byte)(accessBits[2] >>> 4 & 1), (byte) (accessBits[2] >>> 5 & 1), (byte) (accessBits[2] >>> 6 & 1), (byte) (accessBits[2] >>> 7 & 1)} //C3
+            };
+        }catch(IndexOutOfBoundsException e){
+            return null;
+        }
+    }
 }
